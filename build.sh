6e51126f2eb5bba7a0ad4ff1005681e3cffe763b
mneubrand <markus.neubrand@gmail.com>
2012-10-09 09:34:35 -0700

    Updated version with all of @maettig's improvements

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Compress JavaScript
-java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js SfxrParams.js --js SfxrSynth.js --js_output_file jsfxr.min.js
+java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js jsfxr.js --js_output_file jsfxr.min.js
 
 # Check file size
 du -b jsfxr.min.js
