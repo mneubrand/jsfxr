f06b38f2429a759bba76e3e51cab2807745e523b
Gabor Heja <gheja@cubeone>
2013-09-19 22:06:33 +0000

    Modified build.sh to remove linebreaks - saves 7 bytes after Closure Compiler.

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Compress JavaScript
-java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js jsfxr.js --js_output_file jsfxr.min.js
+java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js jsfxr.js | \
+	tr -d '\n\r' > jsfxr.min.js
 
 # Check file size
 du -b jsfxr.min.js
