560c8c9489304fbe798fda40e49fcddeb4300d60
Markus Neubrand <markus.neubrand@gmail.com>
2013-09-23 12:00:02 -0700

    Added extern definition to avoid usage of x

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # WARNING: the following assumes that the variable "x" is not used in jsfxr() _after_ the Closure Compiler
 
 # Compress JavaScript
-java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js jsfxr.js | \
+java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --externs externs.js --js jsfxr.js | \
 	tr -d '\n\r' |
 	sed 's!"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\+/"!x!g' | \
 	sed 's!"data:audio/wav;base64,"!"data:audio/wav;base64,",x="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"!g' > jsfxr.min.js
