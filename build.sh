67aaccbe22a5477ce463f3661968cad6a35bafb6
Markus Neubrand <markus.neubrand@gmail.com>
2013-09-23 11:57:31 -0700

    Made build.sh portable. Don't rely on GNU extensions

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@
 # Compress JavaScript
 java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js jsfxr.js | \
 	tr -d '\n\r' |
-	sed -r 's!"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\+/"!x!g' | \
-	sed -r 's!"data:audio/wav;base64,"!"data:audio/wav;base64,",x="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"!g' > jsfxr.min.js
+	sed 's!"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\+/"!x!g' | \
+	sed 's!"data:audio/wav;base64,"!"data:audio/wav;base64,",x="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"!g' > jsfxr.min.js
 
 # Check file size
-du -b jsfxr.min.js
+wc -c jsfxr.min.js
